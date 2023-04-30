723fbd508700cad5557cb7c1a03bd5539dd7c2d8
anish-gharti <anishgharti10@gmail.com>
2023-04-30 17:31:18 +0545

    routes initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:catalog_app/home_page.dart';
+import 'package:catalog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/login': (context) => const LoginPage()
+      },
     );
   }
 }
